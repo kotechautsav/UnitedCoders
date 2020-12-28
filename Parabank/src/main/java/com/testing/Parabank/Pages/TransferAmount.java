8a2988b07a3ed6b08c493695db62b1873d063d1b
tanmoydebnath2020 <73513550+tanmoydebnath2020@users.noreply.github.com>
2020-12-28 17:49:26 +0530

    Transfer Account 3rd Commit

--- Parabank/src/main/java/com/testing/Parabank/Pages/TransferAmount.java
+++ Parabank/src/main/java/com/testing/Parabank/Pages/TransferAmount.java
@@ -11,16 +11,23 @@ public class TransferAmount {
 	
 	public static String transferringFunds(WebDriver driver) throws Exception {
 		
+	
 		driver.findElement(By.linkText("Transfer Funds")).click();
 		System.out.println("Method is working.");
+		
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("//input[@id='amount']")).sendKeys("20000");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.MILLISECONDS);
-		Select account1 = new Select(driver.findElement(By.xpath("//select[@id='fromAccountId']")));
+		
+		Select account1 = new Select(driver.findElement(By.id("fromAccountId")));
 		account1.selectByIndex(0);
-		Select account2 = new Select(driver.findElement(By.xpath("//select[@id='toAccountId']")));
+		Select account2 = new Select(driver.findElement(By.id("toAccountId")));
 		account2.selectByIndex(0);
 		System.out.println("Account Selected");
-		String ssString = driver.findElement(By.xpath("//*[@id=\'rightPanel\']/div")).getText();
+		
+		driver.findElement(By.xpath("//input[@class=\"button\"]")).click();
+		
+		Thread.sleep(5000);
+		String ssString = driver.findElement(By.xpath("//div[@class=\"ng-scope\"]/p[1]")).getText();
 		return ssString;
 	}
 }
