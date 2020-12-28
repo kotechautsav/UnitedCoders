21034c30a1ac39da6110f21f54b0973e75197e7f
tanmoydebnath2020 <73513550+tanmoydebnath2020@users.noreply.github.com>
2020-12-28 16:54:54 +0530

    Transfer Account 2nd Commit

--- Parabank/src/main/java/com/testing/Parabank/Pages/TransferAmount.java
+++ Parabank/src/main/java/com/testing/Parabank/Pages/TransferAmount.java
@@ -11,7 +11,7 @@ public class TransferAmount {
 	
 	public static String transferringFunds(WebDriver driver) throws Exception {
 		
-		driver.findElement(By.xpath("//a[@href ='/parabank/transfer.htm']")).click();
+		driver.findElement(By.linkText("Transfer Funds")).click();
 		System.out.println("Method is working.");
 		driver.findElement(By.xpath("//input[@id='amount']")).sendKeys("20000");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.MILLISECONDS);
@@ -20,6 +20,7 @@ public class TransferAmount {
 		Select account2 = new Select(driver.findElement(By.xpath("//select[@id='toAccountId']")));
 		account2.selectByIndex(0);
 		System.out.println("Account Selected");
-		return "Successful";
+		String ssString = driver.findElement(By.xpath("//*[@id=\'rightPanel\']/div")).getText();
+		return ssString;
 	}
 }
