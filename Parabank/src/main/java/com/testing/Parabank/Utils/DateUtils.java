2dedc8dafc3bdfd383246f70511c6f275ac19b2b
873397 <873397@PCIN468552.cts.com>
2020-12-28 10:18:22 +0530

    date

--- Parabank/src/main/java/com/testing/Parabank/Utils/DateUtils.java
+++ Parabank/src/main/java/com/testing/Parabank/Utils/DateUtils.java
@@ -1,5 +1,11 @@
 package com.testing.Parabank.Utils;
 
+import java.util.Date;
+
 public class DateUtils {
+	public static String getTimeStamp(){
+		Date date = new Date();
+		return date.toString().replaceAll(":", "_");
+			}
 
 }
